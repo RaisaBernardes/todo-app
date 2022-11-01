821b84a806047dd317907a44643b3196ac0e7d15
Raisa Bernardes <raisa.rmro@gmail.com>
2022-11-01 16:21:29 -0300

    create section done (simplified)

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/*
+<div class="card-header d-flex">                               
+            <input type="checkbox">
+            <button class="btn text-dark text-start border-0 btn-css" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+                Wash the dishes
+            </button> 
+            <a href="#" data-bs-toggle="tooltip" title="edit"><i class="fa-solid fa-pencil pt-3 pe-3 fa-sm icon-pencil"></i></a> 
+            <a href="#" data-bs-toggle="tooltip" title="delete"><i class="fa-solid fa-trash pt-3 fa-sm icon-garbage"></i></a>                              
+</div>
+
+
+<div class="collapse show " id="collapseOne" data-bs-parent="#accordion">
+    <div class="card-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ante est, pharetra nec consequat id
+    </div>
+</div>
+
+*/
+
+// $(document).ready(function (){
+//     $("#todoList").html(`
+//     <div class="card-header d-flex">                               
+//         <input type="checkbox">
+//         <button class="btn text-dark text-start border-0 btn-css" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+//             Wash the dishes
+//         </button> 
+//         <a href="#" data-bs-toggle="tooltip" title="edit"><i class="fa-solid fa-pencil pt-3 pe-3 fa-sm icon-pencil"></i></a> 
+//         <a href="#" data-bs-toggle="tooltip" title="delete"><i class="fa-solid fa-trash pt-3 fa-sm icon-garbage"></i></a>                              
+//     </div>
+//     `)
+// })
+
+
+
+$("#btnNewTask").click(function(){
+
+    let varInput = $(`<input type="text">`);
+    $("#todoList").append(varInput);
+
+    $(varInput).keypress(function(event){
+
+        let task = $(varInput).val();
+        
+        if (event.which === 13){
+
+            $("#todoList").append(`
+                <div class="card-header d-flex">                               
+                    <input type="checkbox">
+                    <button class="btn text-dark text-start border-0 btn-css" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+                      `  + task + `
+                    </button> 
+                    <a href="#" data-bs-toggle="tooltip" title="edit"><i class="fa-solid fa-pencil pt-3 pe-3 fa-sm icon-pencil"></i></a> 
+                    <a href="#" data-bs-toggle="tooltip" title="delete"><i class="fa-solid fa-trash pt-3 fa-sm icon-garbage"></i></a>                              
+                </div>
+            `)
+            $(varInput).remove();
+        }
+    })
+    
+})
+
