433bbf749ff458b79cebea58d5df2a00b0cc3542
Raisa Bernardes <raisa.rmro@gmail.com>
2022-11-02 00:57:31 -0300

    deleting code done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-'use strict';
 
 /*
 <div class="card-header d-flex">                               
@@ -19,45 +18,49 @@
 
 */
 
-// $(document).ready(function (){
-//     $("#todoList").html(`
-//     <div class="card-header d-flex">                               
-//         <input type="checkbox">
-//         <button class="btn text-dark text-start border-0 btn-css" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
-//             Wash the dishes
-//         </button> 
-//         <a href="#" data-bs-toggle="tooltip" title="edit"><i class="fa-solid fa-pencil pt-3 pe-3 fa-sm icon-pencil"></i></a> 
-//         <a href="#" data-bs-toggle="tooltip" title="delete"><i class="fa-solid fa-trash pt-3 fa-sm icon-garbage"></i></a>                              
-//     </div>
-//     `)
-// })
-
-
-
-$("#btnNewTask").click(function(){
-
-    let varInput = $(`<input type="text">`);
-    $("#todoList").append(varInput);
-
-    $(varInput).keypress(function(event){
-
-        let task = $(varInput).val();
-        
-        if (event.which === 13){
-
-            $("#todoList").append(`
-                <div class="card-header d-flex">                               
-                    <input type="checkbox">
-                    <button class="btn text-dark text-start border-0 btn-css" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
-                      `  + task + `
-                    </button> 
-                    <a href="#" data-bs-toggle="tooltip" title="edit"><i class="fa-solid fa-pencil pt-3 pe-3 fa-sm icon-pencil"></i></a> 
-                    <a href="#" data-bs-toggle="tooltip" title="delete"><i class="fa-solid fa-trash pt-3 fa-sm icon-garbage"></i></a>                              
-                </div>
-            `)
-            $(varInput).remove();
-        }
-    })
-    
+//DELETING
+$(document).ready(function(event){
+    $("#todoList").on('click', '.icon-garbage', function(){
+        $(this).parent().fadeOut(200, function(){
+            $(this).parent().remove();
+        })  
+        event.stopPropagation();
+    }) 
 })
+
+
+//CREATING
+
+$(document).ready(function(){
+
+    $("#btnNewTask").click(function(){
+
+        let varInput = $(`<input type="text">`);
+        $("#todoList").append(varInput);
+        $(varInput).keypress(function(event){
+    
+            let task = $(varInput).val();
+            if (event.which === 13){
+    
+                $("#todoList").append(`
+                    <div class="card-header d-flex" id="cardHeader">                               
+                        <input type="checkbox">
+                        <button class="btn text-dark text-start border-0 btn-css" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+                          `  + task + `
+                        </button> 
+                        <a href="#" data-bs-toggle="tooltip" title="edit"><i class="fa-solid fa-pencil pt-3 pe-3 fa-sm icon-pencil"></i></a> 
+                        <a href="#" data-bs-toggle="tooltip" title="delete"><i class="fa-solid fa-trash pt-3 fa-sm icon-garbage"></i></a>                              
+                    </div>
+                `)
+                $(varInput).remove();
+            }
+        })  
+    })
+})
+
+
+
+
+
+
 
