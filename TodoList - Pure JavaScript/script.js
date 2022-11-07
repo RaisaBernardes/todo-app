2cbc1eadb00555bd906d0651e33e491283d09e5e
Raisa Bernardes <raisa.rmro@gmail.com>
2022-11-07 15:54:24 -0300

    index logic added

--- TodoList - Pure JavaScript/script.js
+++ TodoList - Pure JavaScript/script.js
@@ -6,13 +6,13 @@ let database = [
     {'task': 'Task 3', 'status': 'checked'}
 ]
 
-const createItem = (text, status) =>{
+const createItem = (text, status, index) =>{
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
-        <input type="checkbox" ${status}>
+        <input type="checkbox" ${status} data-index=${index}>
         <div>${text}</div>
-        <input type="button" value="x">
+        <input type="button" value="x" data-index=${index}>
     `
     document.getElementById('todoList').appendChild(item);
 }
@@ -26,7 +26,7 @@ const cleanTasks  = () => {
 
 const render = () => {
     cleanTasks();
-    database.forEach (item => createItem(item.task, item.status))
+    database.forEach ((item, index) => createItem(item.task, item.status, index))
 }
 
 const insertItem = (event) => {
@@ -39,7 +39,37 @@ const insertItem = (event) => {
     }
 }
 
+const removeItem = (index) => {
+    database.splice (index, 1);
+    render();
+}
+
+const updateItem = (index) => {
+    if(database[index].status === ''){
+        database[index].status = 'checked';
+        render();
+    }else{
+        database[index].status = '';
+        render();
+    }
+    //database[index].status = database[index].status === '' ? 'checked' : '';
+    //render();
+}
+
+//atualizando banco após interação com a interface
+const clickItem = (event) => {
+    const element = event.target; //returns html element that was clicked
+    if (element.type === 'button'){
+        const index = element.dataset.index; //getting the "data-index" property
+        removeItem(index);
+    } else if(element.type === 'checkbox'){
+        const index = element.dataset.index; 
+        updateItem(index);
+    }
+}
+
 document.querySelector('#newItem').addEventListener('keypress', insertItem)
+document.querySelector('#todoList').addEventListener('click', clickItem)
 
 
 
